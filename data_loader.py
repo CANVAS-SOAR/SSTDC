b9629395bda50e6e19c809c4cd1ecc79e4825a08
Ryan <ashbau12@msu.edu>
2017-06-01 13:43:10 -0400

    Changed the name of data to record

--- data_loader.py
+++ data_loader.py
@@ -7,20 +7,20 @@ class DataLoader:
 	def __init__(self):
 
 		# naming convention of how training and test data and gts are saved
-		self.trainSpecsLoc="./specs/train/*"
+		self.trainRecordLoc="./record/train/*"
 		self.trainGTLoc="./gt/train/*"
-		self.testSpecsLoc="./specs/test/*"
+		self.testRecordLoc="./record/test/*"
 		self.testGTLoc="./gt/test/*"
 
 		# will store lists of the file names
-		self.trainSpecsFiles = None
+		self.trainRecordFiles = None
 		self.trainGTFiles = None
-		self.testSpecsFiles = None
+		self.testRecordFiles = None
 		self.testGTFiles = None	
 
-		self.trainSpecs = None
+		self.trainRecord = None
 		self.trainGT = None
-		self.testSpecs = None
+		self.testRecord = None
 		self.testGT = None
 
 		self.batchIndex = 0	# start at the beginning
@@ -35,15 +35,15 @@ class DataLoader:
 	'''
 	def loadData(self):
 		# makes np.array of the file names of all training and test data
-		self.trainSpecsFiles = np.array(glob(self.trainSpecsLoc))
+		self.trainRecordFiles = np.array(glob(self.trainRecordLoc))
 		self.trainGTFiles = np.array(glob(self.trainGTLoc))
-		self.testSpecsFiles = np.array(glob(self.testSpecsLoc))
+		self.testRecordFiles = np.array(glob(self.testRecordLoc))
 		self.testGTFiles = np.array(glob(self.testGTLoc))
 
 		# loads the data, SAVED AS NP.ARRAYS
-		self.trainSpecs = np.stack([np.load(file) for file in self.trainSpecsFiles])
+		self.trainRecord = np.stack([np.load(file) for file in self.trainRecordFiles])
 		self.trainGT = np.stack([np.load(file) for file in self.trainGTFiles])
-		self.testSpecs = np.stack([np.load(file) for file in self.testSpecsFiles])
+		self.testRecord = np.stack([np.load(file) for file in self.testRecordFiles])
 		self.testGT = np.stack([np.load(file) for file in self.testGTFiles])		
 
 
@@ -51,45 +51,45 @@ class DataLoader:
 	Name = getBatch
 	Purpose = to grab training data with the corresponding ground truth labels
 	Parameters = itself, size = how much data desired, rand = randomly choose or not
-	Returns = an array of [batchSpecs = data np.array, batchGT = ground truth np.array
+	Returns = an array of [batchRecord = data np.array, batchGT = ground truth np.array
 	'''
 	def getBatch(self, size, rand=False):
 
-		batchSpecs = None
+		batchRecord = None
 		batchGT = None
 		
 		if(rand):	# choose index randomly
-			index = np.random.randint(self.trainSpecs.shape[0], size=size)
+			index = np.random.randint(self.trainRecord.shape[0], size=size)
 		else:	# assign index based on batch index
 			# indices to grab are from the current batchIndex plus the size
 			index = range(self.batchIndex, self.batchIndex + size)
-			index = np.remainder(index, self.trainSpecs.shape[0])
+			index = np.remainder(index, self.trainRecord.shape[0])
 			# keep track of where batchIndex should be for next time
-			self.batchIndex = (self.batchIndex + size) % self.trainSpecs.shape[0]
+			self.batchIndex = (self.batchIndex + size) % self.trainRecord.shape[0]
 		
 		# fill arrays to be returned
-		batchSpecs = self.trainSpecs[[i for i in index]]
+		batchRecord = self.trainRecord[[i for i in index]]
 		batchGT = self.trainGT[[i for i in index]]
 		
-		return [batchSpecs, batchGT]
+		return [batchRecord, batchGT]
 
 	'''
 	Name = getTest
 	Purpose = to grab test data with the corresponding ground truth labels
 	Parameters = itself, size = how much data desired, all = all test data or random
-	Returns = an array of [test_specs = data np.array, test_gt = ground truth np.array
+	Returns = an array of [test_record = data np.array, test_gt = ground truth np.array
 	'''
 	def getTest(self, size, all=False):
 
-		test_specs = None
+		test_record = None
 		test_gt = None
 		
 		if(all): # use all data, so just return all data
-			test_specs = self.testSpecs
+			test_record = self.testRecord
 			test_gt = self.testGT
 		else:	# randomly choose ***not completed yet!!*** only works for all
-			index = np.random.randint(self.trainSpecs.shape[0], size=size)
+			index = np.random.randint(self.trainRecord.shape[0], size=size)
 
-		return [test_specs, test_gt]
+		return [test_record, test_gt]
 		
 
